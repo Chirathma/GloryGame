ca7d67bd492ab877d069cac93f6ea764d1ed4dd5
Sudarshana Panditha <sudarshana.scholar@gmail.com>
2018-05-18 15:26:55 +0530

    Add LetterValueElement class

---
+++ src/ie/ucd/GlorySchema/LetterValueElement.java
@@ -0,0 +1,7 @@
+package ie.ucd.GlorySchema;
+
+public class LetterValueElement extends VariableElement {
+    public LetterValueElement(String variableName) {
+        super(variableName);
+    }
+}

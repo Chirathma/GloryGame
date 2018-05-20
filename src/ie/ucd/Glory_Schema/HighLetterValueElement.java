fe917893752f39c67b6384a17b8693e64490ce14
Sudarshana Panditha <sudarshana.scholar@gmail.com>
2018-05-20 21:17:47 +0530

    Add HighLetterValueElement class

---
+++ src/ie/ucd/Glory_Schema/HighLetterValueElement.java
@@ -0,0 +1,7 @@
+package ie.ucd.Glory_Schema;
+
+public class HighLetterValueElement extends LetterValueElement {
+    public HighLetterValueElement(char letter, int value) {
+        super(letter, value);
+    }
+}

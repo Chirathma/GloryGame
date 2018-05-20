dc76c31bd1c6e362322a88aabbfd9e3579db846a
Sudarshana Panditha <sudarshana.scholar@gmail.com>
2018-05-20 16:37:43 +0530

    Implement LetterValueElement

--- src/ie/ucd/GlorySchema/LetterValueElement.java
+++ src/ie/ucd/GlorySchema/LetterValueElement.java
@@ -1,7 +1,19 @@
 package ie.ucd.GlorySchema;
 
-public class LetterValueElement extends VariableElement {
-    public LetterValueElement(String variableName) {
-        super(variableName);
+public class LetterValueElement extends GloryElement {
+    private char mLetter;
+    private int mValue;
+
+    public LetterValueElement(char letter, int value) {
+        mLetter = letter;
+        mValue = value;
+    }
+
+    public char getLetter() {
+        return mLetter;
+    }
+
+    public int getValue() {
+        return mValue;
     }
 }
--- src/ie/ucd/Main.java
+++ src/ie/ucd/Main.java
@@ -1,8 +1,33 @@
 package ie.ucd;
 
+import ie.ucd.GlorySchema.ConstantElement;
+import ie.ucd.GlorySchema.InitialLetterValueElement;
+import ie.ucd.GlorySchema.LetterValueElement;
+import ie.ucd.Score.Scorer;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("Glory Game!");
+
+        Scorer scorer = new Scorer();
+
+        ConstantElement size = new ConstantElement(11);
+
+        LetterValueElement [] elements = new LetterValueElement[size.getValue()];
+
+        for(int i = 0; i < 1 ; ++i){
+            elements[i] = new InitialLetterValueElement('a', 3);
+        }
+
+        for(int i = 1; i < size.getValue() ; ++i){
+            elements[i] = new LetterValueElement('b', 1);
+        }
+
+        int score = scorer.getScore(elements);
+
+        System.out.println("score is " + score);
+
     }
 }

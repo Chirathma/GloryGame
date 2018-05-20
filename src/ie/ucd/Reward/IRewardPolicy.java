f6f45bf1395a4cdcbc24243f47c5d0c75677e45e
Sudarshana Panditha <sudarshana.scholar@gmail.com>
2018-05-20 21:43:03 +0530

    Modify IRewardPolicy interface

--- src/ie/ucd/Reward/IRewardPolicy.java
+++ src/ie/ucd/Reward/IRewardPolicy.java
@@ -2,7 +2,8 @@ package ie.ucd.Reward;
 
 import java.util.List;
 import ie.ucd.Glory_Schema.LetterValueElement;
+import ie.ucd.Glory_Schema.WordElement;
 
 public interface IRewardPolicy {
-    public int getReward(List<LetterValueElement> elements);
+    public int getReward(WordElement wordElement);
 }

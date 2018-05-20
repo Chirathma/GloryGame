9cb957e229b83671bfdd884216f9994dcb0ce274
Sudarshana Panditha <sudarshana.scholar@gmail.com>
2018-05-20 16:38:57 +0530

    Implement InitialLetterValueElement

---
+++ src/ie/ucd/GlorySchema/InitialLetterValueElement.java
@@ -0,0 +1,8 @@
+package ie.ucd.GlorySchema;
+
+public class InitialLetterValueElement extends LetterValueElement{
+
+    public InitialLetterValueElement(char letter, int value) {
+        super(letter, value);
+    }
+}

8fe8c1f09e46c6763135969a8e7b66f621aa6648
Sudarshana Panditha <sudarshana.scholar@gmail.com>
2018-05-20 16:46:50 +0530

    Add and Implement InitialLetterValueReward class

---
+++ src/ie/ucd/Reward/InitialLetterValueReward.java
@@ -0,0 +1,57 @@
+package ie.ucd.Reward;
+
+import ie.ucd.GlorySchema.ConstantElement;
+import ie.ucd.GlorySchema.InitialLetterValueElement;
+import ie.ucd.GlorySchema.LetterValueElement;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class InitialLetterValueReward implements IRewardPolicy {
+    // ConstantElements for multiplying letter values
+    private static final ConstantElement THREE_TIMES = new ConstantElement(3);
+    private static final ConstantElement FIVE_TIMES = new ConstantElement(5);
+    private static final ConstantElement TEN_TIMES = new ConstantElement(10);
+
+
+    private List <LetterValueElement> mElements;
+
+    @Override
+    public int getReward(List<LetterValueElement> elements) {
+
+        // filter out the InitialLetterValueElements
+        findInitialLetters(elements);
+
+        // calculate the Glory points
+        return calculateGloryPoints();
+    }
+
+    private void findInitialLetters(List<LetterValueElement> elements) {
+        // instantiate mElements
+        mElements = new ArrayList<>();
+
+        Predicate<LetterValueElement> predicate = (e) -> e instanceof InitialLetterValueElement;
+        mElements = elements
+                .stream()
+                .parallel()
+                .filter( e -> predicate.test(e))
+                .collect(Collectors.toList());
+    }
+
+    private int calculateGloryPoints() {
+        if(mElements.size() == 1){
+            // three times the initial letter's value
+            return mElements.get(0).getValue() * THREE_TIMES.getValue();
+        }else if (mElements.size() == 2){
+            return mElements.stream().mapToInt( e -> e.getValue()).sum() * FIVE_TIMES.getValue();
+        }else if (mElements.size() == 3){
+            return mElements.stream().mapToInt( e -> e.getValue()).sum() * TEN_TIMES.getValue();
+        }
+
+        return 0;
+    }
+
+
+}

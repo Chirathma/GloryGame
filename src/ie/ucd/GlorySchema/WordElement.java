b3d73c3c7ad3b917c241abd17b061475a058833a
Sudarshana Panditha <sudarshana.scholar@gmail.com>
2018-05-20 19:00:34 +0530

    Modify WordElement class

--- src/ie/ucd/GlorySchema/WordElement.java
+++ src/ie/ucd/GlorySchema/WordElement.java
@@ -1,4 +1,12 @@
 package ie.ucd.GlorySchema;
 
 public class WordElement extends GloryElement {
+    private static final ConstantElement WORDSIZE = new ConstantElement(11);
+
+    // member to hold LetterValueElements of the word
+    private LetterValueElement [] mLetterValueElements;
+
+
+
+
 }

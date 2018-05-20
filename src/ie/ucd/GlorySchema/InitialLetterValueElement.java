f70345eebb6b6bbf197018316a7bd121eb8b9e27
Sudarshana Panditha <sudarshana.scholar@gmail.com>
2018-05-20 19:12:25 +0530

    Modify InitialLetterValueElement class

--- src/ie/ucd/GlorySchema/InitialLetterValueElement.java
+++ src/ie/ucd/GlorySchema/InitialLetterValueElement.java
@@ -1,8 +1,11 @@
 package ie.ucd.GlorySchema;
 
 public class InitialLetterValueElement extends LetterValueElement{
+    // member instance to know the original position of the initial letter
+    private int mInitialPosition;
 
-    public InitialLetterValueElement(char letter, int value) {
+    public InitialLetterValueElement(char letter, int value, int initialPosition) {
         super(letter, value);
+        mInitialPosition = initialPosition;
     }
 }

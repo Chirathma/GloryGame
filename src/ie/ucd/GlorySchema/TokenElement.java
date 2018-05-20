2c7210c22a1d39516ba004a75efdf259883bd74a
Sudarshana Panditha <sudarshana.scholar@gmail.com>
2018-05-20 18:00:17 +0530

    Add TokenElement class

---
+++ src/ie/ucd/GlorySchema/TokenElement.java
@@ -0,0 +1,13 @@
+package ie.ucd.GlorySchema;
+
+public class TokenElement {
+    private String mToken;
+
+    public TokenElement(String token) {
+        mToken = token;
+    }
+
+    public String getToken() {
+        return mToken;
+    }
+}

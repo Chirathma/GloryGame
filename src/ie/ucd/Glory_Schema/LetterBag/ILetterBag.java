2566edf4b3f0992bd30740d8a5d79ce2e6369a74
Devni Perera <devniperera14@gmail.com>
2018-06-24 23:40:14 +0530

    Add ILetterBag

---
+++ src/ie/ucd/Glory_Schema/LetterBag/ILetterBag.java
@@ -0,0 +1,13 @@
+package ie.ucd.Glory_Schema.LetterBag;
+
+/**
+ * Created by Devni on 6/13/2018.
+ */
+public interface ILetterBag{
+
+     char getVowelLetter();
+     char getConsonantLetter();
+     char getInitialLetters();
+     void shuffleLetters();
+
+}

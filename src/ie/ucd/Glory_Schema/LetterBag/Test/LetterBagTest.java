1e02124f8e20726cb4d671f53b75ee9fbbbf2707
Devni Perera <devniperera14@gmail.com>
2018-06-26 23:36:54 +0530

    Add LetterBagTest.java

---
+++ src/ie/ucd/Glory_Schema/LetterBag/Test/LetterBagTest.java
@@ -0,0 +1,39 @@
+package ie.ucd.Glory_Schema.LetterBag.Test;
+
+import org.junit.Test;
+
+/**
+ * Created by Devni on 6/26/2018.
+ */
+public class LetterBagTest {
+    @Test
+    public void getVowelLetter() throws Exception {
+
+    }
+
+    @Test
+    public void getConsonantLetter() throws Exception {
+
+    }
+
+    @Test
+    public void getInitialLetter() throws Exception {
+
+    }
+
+    @Test
+    public void shuffleLetters() throws Exception {
+
+    }
+
+    @Test
+    public void shuffleVowels() throws Exception {
+
+    }
+
+    @Test
+    public void shuffleConsonants() throws Exception {
+
+    }
+
+}
